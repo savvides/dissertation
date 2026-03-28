bb697eb40c7233112c80bdbc928d4d86868f57bd
Philippos Savvides <savvides1@gmail.com>
2026-03-28 12:46:25 -0700

    feat: setup basic scrolltelling layout and styling

---
+++ script.js
@@ -0,0 +1,6 @@
+function init() {
+    // Initialization code for scrolltelling and D3 will go here
+    console.log("Scrolltelling initialized");
+}
+
+document.addEventListener("DOMContentLoaded", init);
