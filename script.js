31543139cceb990d71be37c1efd4954f3dbe1854
Philippos Savvides <savvides1@gmail.com>
2026-03-28 12:51:00 -0700

    fix: resolve spec compliance issues

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function init() {
     // Initialization code for scrolltelling and D3 will go here
-    console.log("Scrolltelling initialized");
 }
 
 document.addEventListener("DOMContentLoaded", init);
